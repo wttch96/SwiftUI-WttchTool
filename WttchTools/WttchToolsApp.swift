b9e13183e76993bd7ca425762fd136a853ff35bc
wttch（王冲） <wttch@wttch.com>
2022-05-25 21:54:46 +0800

    Initial Commit

---
+++ WttchTools/WttchToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WttchToolsApp.swift
+//  WttchTools
+//
+//  Created by Wttch on 2022/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WttchToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
