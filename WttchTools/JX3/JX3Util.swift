1309532c97322b3eaaf813340396522a5ca2ab5d
wttch（王冲） <wttch@wttch.com>
2022-11-08 17:41:22 +0800

    jx3 text 显示demo

--- WttchTools/JX3/JX3Util.swift
+++ WttchTools/JX3/JX3Util.swift
@@ -6,36 +6,88 @@
 //
 
 import Foundation
+import SwiftUI
 
+public struct JX3StyleTextLine: Identifiable {
+    public typealias ID = Int
+    public var id: Int
+    public var texts: [JX3StyleText] = []
+}
+
+public struct JX3StyleText : Identifiable {
+    public typealias ID = Int
+    public var id: Int
+    public var styles: [String: String]
+    public var color: Color? {
+        get {
+            if let rStr = styles["r"], let gStr = styles["g"], let bStr = styles["b"] {
+                if let r = Int(rStr), let g = Int(gStr), let b = Int(bStr) {
+                    return Color(cgColor: CGColor(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: 1))
+                }
+            }
+            return nil
+        }
+    }
+}
+
+
+
+/// https://github.com/JX3BOX/jx3box-common/blob/master/js/utils.js
 public class JX3Util {
+    public static func texts(_ texts: [JX3StyleText]) -> [JX3StyleTextLine] {
+        var result: [JX3StyleTextLine] = []
+        var line : [JX3StyleText] = []
+        for text in texts {
+            let textStr = "\(text.styles["text"]!)".replacingOccurrences(of: "\\n", with: "\n")
+            var t = text
+            t.styles["text"] = textStr.replacingOccurrences(of: "\n$", with: "", options: .regularExpression, range: nil)
+            line.append(t)
+            if textStr.hasSuffix("\n") {
+                result.append(JX3StyleTextLine(id: text.id, texts: line))
+                line = []
+            }
+        }
+        if !line.isEmpty {
+            result.append(JX3StyleTextLine(id: -1, texts: line))
+        }
+        return result
+    }
     
     /// 将游戏内的形如`<Text>text="****" font="**" **=***</text>`的字符串进行解析
     /// 返回一个包含font、text等属性的对象，方便进行进一步的渲染或者其他处理。
-    public static func extractTextContent(_ textStr: String?) -> String {
+    public static func extractTextContent(_ textStr: String?) -> [JX3StyleText] {
         guard let text = textStr else {
-            return ""
+            return []
         }
-        
-        let xmlString = "<xml>" + text + "</xml>"
-        var result = xmlString.replacingOccurrences(of: "<Text>(.*?)</text>", with: "<span $1></span>", options: .regularExpression, range: nil)
-        result  = result.replacingOccurrences(of: "=(\\d+)", with: "='$1'", options: .regularExpression, range: nil)
-        NSLog(result)
-        var ret = [[:]]
+        // xml字符串
+        var xmlString = "<xml>" + text + "</xml>"
+        // 替换<Text>***</text>为<span ***></span>
+        xmlString = xmlString.replacingOccurrences(of: "<Text>(.*?)</text>", with: "<span $1></span>", options: .regularExpression, range: nil)
+        // swift xml库解析时属性的值必须为带引号的
+        xmlString  = xmlString.replacingOccurrences(of: "=(\\d+)", with: "='$1'", options: .regularExpression, range: nil)
+        var results: [JX3StyleText] = []
+        var i = 0
         do {
-            let xml = try XMLDocument(data: result.data(using: .utf8)!)
+            // 提取span的所有属性
+            let xml = try XMLDocument(xmlString: xmlString)
             let spans = xml.rootElement()?.elements(forName: "span")
             for span in spans! {
-                var ret1: [String:String] = [:]
+                var result : [String:String] = [:]
                 for attr in span.attributes ?? [] {
-                    NSLog("\(attr.localName) \(attr.stringValue)")
-                    ret1[attr.localName!] = attr.stringValue!
+                    if let key = attr.localName, let val = attr.stringValue {
+                        result[key] = val
+                    }
                 }
-                ret.append(ret1)
+                if !result.isEmpty {
+                    let styleText = JX3StyleText(id: i, styles: result)
+                    results.append(styleText)
+                    i += 1
+                }
             }
         } catch {
             NSLog(error.localizedDescription)
         }
-        return "<div>\(result)</div>"
+        return results
     }
 }
 
--- WttchTools/JX3/JX3View.swift
+++ WttchTools/JX3/JX3View.swift
@@ -10,15 +10,27 @@ import WebKit
 
 struct JX3View: View {
     @State var text: String = ""
+    @State var textStyle: [JX3StyleText] = []
+    @State var texts: [JX3StyleTextLine] = []
     var body: some View {
         VStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .onAppear {
                     // JX3Api().searchItem("岚尘")
-                    let text = "<Text>text=\"使用：本周每获得5000点战阶积分，可以开启一次瑰石获得如下的装备，每周最多开启二个。\" font=105 </text><Text>text=\"[怒鳞翻江·碎月重剑]\" name=\"iteminfolink\" eventid=513 script=\"this.nVersion=0 this.dwTabType=6 this.dwIndex=27689 this.OnItemLButtonDown=function() OnItemLinkDown(this) end\" font=100 r=255 g=40 b=255 </text><Text>text=\"，\" font=100 </text><Text>text=\"[怒鳞翻江·碎月轻剑]\" name=\"iteminfolink\" eventid=513 script=\"this.nVersion=0 this.dwTabType=6 this.dwIndex=27688 this.OnItemLButtonDown=function() OnItemLinkDown(this) end\" font=100 r=255 g=40 b=255 </text><Text>text=\"。\" font=100 </text>"
-                    NSLog(text)
-                    JX3Util.extractTextContent(text)
+                    let text = "<Text>text=\"使用：本周每获得5000点战阶积分，可以开启一次瑰石\\n获得如下的装备，每周最多开启二个。\\n\" font=105 </text><Text>text=\"[怒鳞翻江·碎月重剑]\" name=\"iteminfolink\" eventid=513 script=\"this.nVersion=0 this.dwTabType=6 this.dwIndex=27689 this.OnItemLButtonDown=function() OnItemLinkDown(this) end\" font=100 r=255 g=40 b=255 </text><Text>text=\"，\" font=100 </text><Text>text=\"[怒鳞翻江·碎月轻剑]\" name=\"iteminfolink\" eventid=513 script=\"this.nVersion=0 this.dwTabType=6 this.dwIndex=27688 this.OnItemLButtonDown=function() OnItemLinkDown(this) end\" font=100 r=255 g=40 b=255 </text><Text>text=\"。\" font=100 </text>"
+                    textStyle = JX3Util.extractTextContent(text)
+                    texts = JX3Util.texts(textStyle)
                 }
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(texts, content: { textLine in
+                    HStack(spacing: 0) {
+                        ForEach(textLine.texts, content: { text in
+                            Text("\(text.styles["text"]!)".replacingOccurrences(of: "\\n", with: "\n"))
+                                .foregroundColor(text.color)
+                        })
+                    }
+                })
+            }
             AttributedText(htmlContent: text)
         }
     }
